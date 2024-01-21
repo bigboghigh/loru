336da25e08f2ab4a5a889de750f8d9d4e5e4003c
bigboghigh <156019142+bigboghigh@users.noreply.github.com>
2024-01-21 14:46:29 +0500

    Update index.js

--- index.js
+++ index.js
@@ -47,5 +47,5 @@ app.get('/', async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(Server is running at http://localhost:${port});
+  console.log('Server is running at http://localhost:${port}');
 });
