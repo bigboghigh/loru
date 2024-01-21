518f5697d6c2b8f4a53175fe8d81a515bd246589
bigboghigh <156019142+bigboghigh@users.noreply.github.com>
2024-01-21 15:57:06 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,52 +2,27 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-app.use(express.json());
+const port = 3000;
 
-app.get('/processCaptcha', async (req, res) => {
+app.get('/getUserCaptcha', async (req, res) => {
   try {
-    let random = null;
-    let key = null;
-    let password = null;
+    // Step 1: Send GET request to get captcha image
+    const captchaImageResponse = await axios.get('https://external-api.agilecdn.cloud/user/api/user/captchaImage');
 
-    while (true) {
-      // Send request to first API to get random value
-      const captchaImageResponse = await axios.get('https://external-api.agilecdn.cloud/user/api/user/captchaImage');
-      random = captchaImageResponse.data.data.random;
+    // Step 2: Extract random from the response
+    const random = captchaImageResponse.data.random;
 
-      // Send request to the second API with the random value
-      const checkCaptchaResponse = await axios.get('https://external-api.agilecdn.cloud/user/api/user/checkCaptcha?point=84&random=${random}');
+    // Step 3: Send GET request to check captcha with the extracted random
+    const checkCaptchaResponse = await axios.get(`https://external-api.agilecdn.cloud/user/api/user/checkCaptcha?point=84&random=${random}`);
 
-      if (checkCaptchaResponse.data.data.data !== null) {
-        key = checkCaptchaResponse.data.data.data;
-        break;
-      }
-    }
-
-    // Send post request to cdn-accountset-apply API
-    const applyCdnAccountResponse = await axios.post('https://external-api.agilecdn.cloud/user/api/user/cdn-accountset-apply?lang=en_US', """{"email":"bajssuus@byom.de","veriCode":${key},"lang":"en","userType":0,"cdnParam":"{\"cdnCacheBehaviorModels\":[{\"cdnCacheBehaviorForwardModel\":{\"cookies\":{\"forward\":\"none\",\"whitelistedNames\":[]},\"headerType\":0,\"headers\":[],\"queryString\":2,\"queryStringCacheKeys\":[]},\"compress\":false,\"defaultTtl\":0,\"fieldLevelEncryptionId\":\"\",\"maxTtl\":31536000,\"minTtl\":0,\"originId\":\"ecaee4e0-b639-11ee-b0c4-db23679837ea-pu.edvin.online\",\"pathPattern\":\"*\",\"smoothStreaming\":false,\"type\":1,\"viewerProtocolPolicy\":\"allow-all\",\"allowedMethods\":3,\"isCacheOptions\":false}],\"cdnCertificateModel\":{\"cdnCertificateId\":\"\",\"minimumProtocolVersion\":\"\",\"sslSupportMethod\":false,\"type\":1,\"cdnConfigId\":\"\",\"id\":\"\"},\"cdnOriginsModels\":[{\"domainName\":\"pu.edvin.online\",\"httpPort\":80,\"httpsPort\":443,\"originCustomHeaders\":[],\"originId\":\"ecaee4e0-b639-11ee-b0c4-db23679837ea-pu.edvin.online\",\"originKeepaliveTimeout\":5,\"originPath\":\"\",\"originProtocolPolicy\":\"match-viewer\",\"originReadTimeout\":30,\"originSslProtocols\":[\"TLSv1\"],\"type\":1,\"originType\":1}],\"comment\":\"\",\"defaultRootObject\":\"\",\"customHostType\":1,\"customHost\":\"\",\"domain\":\"\",\"httpVersion\":\"http2\",\"isIpv6Enabled\":false,\"status\":1,\"type\":1,\"deployStatus\":0,\"platformDomain\":\"\",\"visitRegion\":\"Non-Asia-Pacific\",\"cdnRestrictionModel\":{\"locations\":[\"CN\",\"HK\",\"MO\"],\"restrictionType\":2},\"configModel\":7}"}"""
-    });
-
-    password = applyCdnAccountResponse.data.data.password;
-
-    // Send post request to login API
-    const loginResponse = await axios.post('https://external-api.agilecdn.cloud/user/auth/login', {
-      username: 'jsjssjj@byom.de',
-      password: password,
-      accountNumber: '',
-      channel: 86,
-      code: '',
-      terminal: ''
-    });
-
-    // Return the password received from the last API
-    res.json({ password: password });
+    // Step 4: Return the response to the user
+    res.json(checkCaptchaResponse.data);
   } catch (error) {
-    console.error('Error:', error.message);
-    res.status(500).json({ error: 'An error occurred' });
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
 });
